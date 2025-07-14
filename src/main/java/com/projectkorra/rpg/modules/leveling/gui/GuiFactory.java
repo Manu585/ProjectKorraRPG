12a8197d9b231e0bc15e0d0872ef7755696a6536
Manu585 <manuelgogg2@gmail.com>
2025-07-14 12:59:57 +0200

    Added Lore option

--- src/main/java/com/projectkorra/rpg/modules/leveling/gui/GuiFactory.java
+++ src/main/java/com/projectkorra/rpg/modules/leveling/gui/GuiFactory.java
@@ -1,6 +1,7 @@
 package com.projectkorra.rpg.modules.leveling.gui;
 
 import com.projectkorra.projectkorra.util.ChatUtil;
+import com.projectkorra.rpg.modules.leveling.rpgplayer.RpgPlayer;
 import com.projectkorra.rpg.ui.InventoryUI;
 import com.projectkorra.rpg.ui.builder.InventoryUIBuilder;
 import com.projectkorra.rpg.ui.service.InventoryService;
@@ -9,6 +10,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class GuiFactory {
     private final InventoryService service;
@@ -18,61 +22,59 @@ public class GuiFactory {
     }
 
     public void openMainMenu(Player player) {
-        InventoryUI menu = createMainMenu();
-        service.open(player, menu);
+        service.open(player, createMainMenu(player));
     }
 
-    public InventoryUI createMainMenu() {
-        InventoryUI subMenu1 = InventoryUIBuilder
-                .create(3, ChatUtil.color("&aSub menu 1"))
-                .withButton(4, 1, makeItem(Material.DIAMOND, ChatUtil.color("&a&lCLICK ME")), this::onSubMenu1Click)
-                .withButton(0, 2, makeItem(Material.ARROW, ChatUtil.color("&cBack")), click -> openMainMenu((Player) click.getWhoClicked()))
-                .build();
-
-        InventoryUI subMenu2 = InventoryUIBuilder
-                .create(3, ChatUtil.color("&eSub menu 2"))
-                .withButton(4, 1, makeItem(Material.EMERALD, ChatUtil.color("&e&lCLICK ME")), this::onSubMenu2Click)
-                .withButton(0, 2, makeItem(Material.ARROW, ChatUtil.color("&cBack")), click -> openMainMenu((Player) click.getWhoClicked()))
-                .build();
-
+    public InventoryUI createMainMenu(Player player) {
         InventoryUIBuilder mainMenu = InventoryUIBuilder.create(3, ChatUtil.color("&1Main Menu"));
 
         ItemStack filler = makeItem(Material.GRAY_STAINED_GLASS_PANE, " ");
+        ItemStack vines = makeItem(Material.TWISTING_VINES, " ");
         for (int y = 0; y < mainMenu.getHeight(); y++) {
             for (int x = 0; x < mainMenu.getWidth(); x++) {
+                if (x == 0 || x == 8) {
+                    mainMenu.withItem(x, y, vines);
+                    continue;
+                }
                 mainMenu.withItem(x, y, filler);
             }
         }
 
-        // NOT USING LAMBDA TO SHOW JR. DEVS THE PROPER MULTI LINE WAY
-        mainMenu.withButton(3, 1, makeItem(Material.DIAMOND_BLOCK, ChatUtil.color("&aOpen sub menu 1")), click -> {
-            Player player = (Player) click.getWhoClicked();
-            service.open(player, subMenu1);
-        }).withButton(5, 1, makeItem(Material.EMERALD_BLOCK, ChatUtil.color("&eOpen sub menu 2")), click -> {
-            Player player = (Player) click.getWhoClicked();
-            service.open(player, subMenu2);
-        });
+        RpgPlayer rpgPlayer = new RpgPlayer(player.getUniqueId(), 5, 0); // Test instance
+
+        InventoryUI skillTreeMenu = InventoryUIBuilder
+                .create(3, ChatUtil.color("&6Skilltree"))
+                .withButton(4, 1, makeItem(Material.DIAMOND, ChatUtil.color("&a&lCLICK ME")), this::onSubMenu1Click)
+                .withButton(0, 2, makeItem(Material.ARROW, ChatUtil.color("&cBack")), click -> openMainMenu((Player) click.getWhoClicked()))
+                .build();
+
+        // NOT USING LAMBDA TO SHOW OTHER JR. DEVS THE PROPER MULTI-LINE WAY
+        mainMenu.withButton(3, 1, makeItem(Material.DIAMOND_BLOCK, ChatUtil.color("&1SkillTree")), click -> {
+            Player clicker = (Player) click.getWhoClicked();
+            service.open(clicker, skillTreeMenu);
+        }).withItem(5, 1, makeItem(Material.NETHER_STAR, ChatUtil.color("&bStats"), List.of("Level: " + rpgPlayer.getLevel() + "\n" + "XP: " + rpgPlayer.getXp())));
 
         return mainMenu.build();
     }
 
-    private void onSubMenu1Click(InventoryClickEvent click) {
+    private void onSubMenu1Click(@NotNull InventoryClickEvent click) {
         Player p = (Player) click.getWhoClicked();
         p.sendMessage(ChatUtil.color("&aHello from Sub Menu 1"));
     }
 
-    private void onSubMenu2Click(InventoryClickEvent click) {
-        Player p = (Player) click.getWhoClicked();
-        p.sendMessage(ChatUtil.color("&eHello from Sub Menu 2"));
+    private static @NotNull ItemStack makeItem(Material material, String name) {
+        return makeItem(material, name, List.of());
     }
 
-    private static ItemStack makeItem(Material material, String name) {
+    private static @NotNull ItemStack makeItem(Material material, String name, List<String> lore) {
         ItemStack item = new ItemStack(material);
         ItemMeta meta = item.getItemMeta();
-        if (meta == null) {
-            throw new IllegalStateException("Cannot get ItemMeta for " + material);
-        }
+        if (meta == null) throw new IllegalStateException("Cannot get ItemMeta for " + material);
+
         meta.setDisplayName(name);
+        if (!lore.isEmpty()) {
+            meta.setLore(lore);
+        }
         item.setItemMeta(meta);
         return item;
     }
