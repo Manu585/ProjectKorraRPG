e8a1021db5d8b803086be0a87095109d7ce33041
Manu585 <manuelgogg2@gmail.com>
2025-07-14 16:20:11 +0200

    Small formatting changes, added helper methods, converted RpgPlayer to Record (not sure if permanently or temporarily)

--- src/main/java/com/projectkorra/rpg/modules/leveling/rpgplayer/RpgPlayer.java
+++ src/main/java/com/projectkorra/rpg/modules/leveling/rpgplayer/RpgPlayer.java
@@ -6,28 +6,7 @@ import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
-public class RpgPlayer {
-    private final UUID uuid;
-    private final int level;
-    private final double xp;
-
-    public RpgPlayer(UUID uuid, int level, double xp) {
-        this.uuid = uuid;
-        this.level = level;
-        this.xp = xp;
-    }
-
-    public UUID getUuid() {
-        return uuid;
-    }
-
-    public int getLevel() {
-        return this.level;
-    }
-
-    public double getXp() {
-        return this.xp;
-    }
+public record RpgPlayer(UUID uuid, int level, double xp) {
 
     private Player asPlayer() {
         return Bukkit.getPlayer(uuid);
